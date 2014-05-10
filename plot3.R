7f4002389873a08bfc79cb23b71b0a0fbbfb44cb
HunBug <akos.strack+github@gmail.com>
2014-05-10 21:33:43 +0200

    Plot3 added

---
+++ plot3.R
@@ -0,0 +1,21 @@
+#Load data and initialize header
+data <- read.table("household_power_consumption.txt",header=TRUE,skip=66637,nrows=2879,sep=";")
+header <- read.table("household_power_consumption.txt",header=TRUE,nrows=1,sep=";")
+names(data) <- names(header)
+
+#Convert data types
+#data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+#data$Time <- strptime(data$Time,format="%H:%M:%S")
+data$DateTime <- strptime(paste(data$Date,data$Time,sep=" "),format="%d/%m/%Y %H:%M:%S")
+
+#draw
+png("plot3.png", width = 480, height = 480)
+#ONLY WORKS IN ENGLISH ENVIRONMENT
+#plot(data$DateTiem,data$Sub_metering_1,type="l",xlab="",ylab='Energy sub metering', col="black")
+
+plot(data$Sub_metering_1,type="l",xlab="",ylab='Energy sub metering',xaxt="n", col="black")
+lines(data$Sub_metering_2,col="red")
+lines(data$Sub_metering_3,col="blue")
+axis(1, at=c(1,length(data$Global_active_power)/2+1,length(data$Global_active_power)),labels=c("Thu","Fri","Sat"))
+legend("topright",lwd=1,lty=c(1,1,1),col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
