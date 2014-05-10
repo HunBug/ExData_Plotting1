793e037729344d5bc8b8ad0d9325334a208aeffa
HunBug <akos.strack+github@gmail.com>
2014-05-10 21:09:25 +0200

    Plot2 added, plot1 fixed

--- plot2.R
+++ plot2.R
@@ -9,6 +9,9 @@ names(data) <- names(header)
 data$DateTime <- strptime(paste(data$Date,data$Time,sep=" "),format="%d/%m/%Y %H:%M:%S")
 
 #draw
-png("plot1.png", width = 480, height = 480)
-hist(data$Global_active_power,main="Global Active Power",xlab ="Global Active Power (kilowatts)",col="red")
+png("plot2.png", width = 480, height = 480)
+#ONLY WORKS IN ENGLISH ENVIRONMENT
+#plot(data$DateTime,data$Global_active_power,type="l",xlab="")
+plot(data$Global_active_power,type="l",xlab="",ylab='Global Active Power (kilowatts)',xaxt="n")
+axis(1, at=c(1,length(data$Global_active_power)/2+1,length(data$Global_active_power)),labels=c("Thu","Fri","Sat"))
 dev.off()
